414df4aba93f506228d25ffb987dbd5a7240ad42
sunandad2090 <149049323+sunandad2090@users.noreply.github.com>
2024-02-04 16:12:45 +0530

    Update setup.sh

--- common/setup.sh
+++ common/setup.sh
@@ -1,1 +1,2 @@
 kubectl create ns common
+kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=test --docker-password=test --docker-email=sunandad2090@gmail.com -n common
