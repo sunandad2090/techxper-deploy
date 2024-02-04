c7af1129f4b44b02332c5ed2fb13730a74854f8a
sunandad2090 <149049323+sunandad2090@users.noreply.github.com>
2024-02-04 15:23:33 +0530

    Update setup.sh

--- cert-manager/setup.sh
+++ cert-manager/setup.sh
@@ -1,1 +1,3 @@
+#https://cloud.yandex.com/en/docs/managed-kubernetes/tutorials/ingress-cert-manager#install-certs-manager
+
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.1/cert-manager.yaml
