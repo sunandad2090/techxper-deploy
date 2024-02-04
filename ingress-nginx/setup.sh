0f297e0e6d5ca5bb9e3255449820209b7180b0e5
sunandad2090 <149049323+sunandad2090@users.noreply.github.com>
2024-02-04 15:32:10 +0530

    Update setup.sh

--- ingress-nginx/setup.sh
+++ ingress-nginx/setup.sh
@@ -1,5 +1,5 @@
 # https://cloud.yandex.com/en/docs/managed-kubernetes/tutorials/ingress-cert-manager#install-controller
-
+kubectl create ns ingress-nginx
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx && \
 helm repo update && \
 helm install ingress-nginx ingress-nginx/ingress-nginx
