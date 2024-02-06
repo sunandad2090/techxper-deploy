d03973ab4cbc1966fa6a89cb58de9850a4bd5bc8
sunandad2090 <149049323+sunandad2090@users.noreply.github.com>
2024-02-06 11:52:53 +0530

    Update setup.sh

--- common/jenkins/setup.sh
+++ common/jenkins/setup.sh
@@ -1,3 +1,6 @@
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
 helm upgrade --install myjenkins jenkins/jenkins
+kubectl exec --namespace common -it  jenkins0 -- /bin/cat /run/secrets/chart-admin-password && echo
+ kubectl --namespace default port-forward svc/myjenkins 8080:8080
+
